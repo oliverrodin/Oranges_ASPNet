492fe25beb3308cd95d80fa57366bc665abf6532
Pontus Hylander <pontus.hylander@edu.edugrade.se>
2022-05-30 15:44:57 +0200

    ContactUs

---
+++ Oranges_ASPNet/Controllers/ContactUsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Oranges_ASPNet.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
